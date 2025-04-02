b53a4ad780d22667d265a75f4609d82ba0a02a35
Shaheen Shaikh <162513187+FoxNineOne@users.noreply.github.com>
2025-04-02 20:35:36 +0100

    Wednesday Night Push

--- 12-Numbers-Dates-Timers-Bankist/starter/script_lecture.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script_lecture.js
@@ -10,7 +10,7 @@
 // DIFFERENT DATA! Contains movement dates, currency and locale
 
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Shaheen Shaikh',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -265,7 +265,7 @@ btnSort.addEventListener('click', function (e) {
 
 //Specific or financial calculations in JS can be difficult
 // console.log(0.1 + 0.2 === 0.3); //will return false
-
+/*
 // convert String to number
 console.log(Number('23'));
 console.log(+'23'); //will type convert to Number .. Looks cleaner!
@@ -302,3 +302,116 @@ console.log(Number.isFinite('20')); // false - it is string.
 console.log(Number.isInteger(206)); // true - is integer
 console.log(Number.isInteger(20.67)); // false - is decimal
 console.log(Number.isInteger('20')); // false - it is string.
+*/
+
+//Math and Rounding Numbhers
+/*
+//Square Root
+//console.log(Math.sqrt(25));
+//console.log(25 ** (1 / 2));
+//console.log(8 ** (1 / 3)); // cubic root
+
+//Return Max and Minimum
+//console.log(Math.max(5, 18, '43', 11, 2)); //will also type convert, but will not parse from string with non number characters
+//console.log(Math.min(5, 18, '43', 11, 2));
+
+// Return Pi
+//console.log(Math.PI);
+
+// Calculate the area of a circle with a radius of 10 pixels
+//console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+//Random function - return a number between 1 and 6
+//console.log(Math.trunc(Math.random() * 6) + 1);
+
+// Number Generator between two ranges.
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+console.log(randomInt(10, 20));
+
+console.log(randomInt(0, 3));
+
+// Rounding integers
+console.log(Math.trunc(23.3)); //truncates decimals away.
+
+console.log(Math.round(23.3)); //Rounds to nearest integer
+console.log(Math.round(23.6)); //Rounds to nearest integer
+
+console.log(Math.ceil(23.3)); //Returns 24.. ceil rounds up to next integer
+console.log(Math.ceil(23.6)); //Returns 24.. ceil rounds up to next integer
+
+console.log(Math.floor('23.3')); //Returns 23.. ceil rounds down to next integer
+console.log(Math.floor(23.6)); //Returns 23.. ceil rounds down to next integer
+
+// Truncate and Floor do same when dealing with positive numbers
+// however, for negative numbers, it doesn't work this way.
+
+console.log(Math.trunc(-23.3)); //Returns -23
+console.log(Math.floor(-23.3)); //Returns -24  floor rounds down remember!
+// Floor's behaviour is consistent with negative or positive
+
+// Rounding floating point (decimals)
+// we have to specify decimals in parenthesis (brackets for fancy pants people)
+console.log((2.7).toFixed(0)); //returns 3, as a string (not an inteegr)
+console.log((2.7).toFixed(3)); //returns 2.700, as we specified 3 after decimal
+console.log((2.745).toFixed(2)); //returns 2.75, rounds up?
+console.log((2.744).toFixed(2)); //returns 2.75, rounds down?
+
+
+*/
+
+// The remainder operator (might be useful when batching?)
+/*
+// This guy says reminder.... then remainder..
+// wrong or right.. pick one and be consistent mate
+
+console.log(5 % 2); //returns 1, which is remainder (not divsible value)
+console.log(5 / 2); // returns 2.5.. as it should
+
+console.log(8 % 3); // returns 2
+console.log(8 / 3); // returns 2.6666666665
+
+// odd and even numbers
+// even numbers have no remainder
+console.log(6 % 2 === 0); //returns true, it is even
+console.log(61 % 2 === 0); //returns false it is odd
+
+//function it!
+const isEven = n => n % 2 === 0;
+console.log(isEven(4));
+console.log(isEven(47));
+console.log(isEven(66));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (r, i) {
+    if (i % 2 === 0) r.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) r.style.backgroundColor = 'lightblue';
+    if (i % 5 === 0) r.style.backgroundColor = 'purple';
+  });
+});
+
+
+*/
+
+// Numberic Separators
+
+// 287,760,000,000
+const diameter = 287_460_000_000; // We can use separators to help read large numbers
+console.log(diameter); //returns 287460000000 - js ignores the separators
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+console.log(transferFee1);
+console.log(transferFee2);
+
+const PI = 3.14_15; //Cannot place underscore at beginning, end, consecutive, or next to decimal
+console.log(PI);
+
+console.log(Number('230000')); // works
+console.log(Number('230_000')); //returns NaN
+console.log(parseInt('230_000')); //returns 230, the 000 is ignored.
+// Only use numeric separators when dealing with numbers..
+// Numbers as strings should not use underscores.
